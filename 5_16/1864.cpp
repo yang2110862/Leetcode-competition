777752f7e1b9b2dca434db70e28a0f17b7a7d70a
yang2110862 <1563782600@qq.com>
2021-05-20 15:14:46 +0800

    5/20

--- 5_16/1864.cpp
+++ 5_16/1864.cpp
@@ -1,51 +1,57 @@
 #include "header.h"
- //改天再写
+
 class Solution {
+private:
+    int num1;
+    int num0;
 public:
     int minSwaps(string s) {
-        int n = s.size();
-        if (n == 1) {
-            return 0;
+        int len = s.size();
+        for (auto c : s) {
+            if (c == '1') ++num1;
+            else ++num0;
         }
-        int cnt1 = 0, cnt0 = 0;
-        int num0 = 0;
-        if (n % 2 == 0) {
-            for (int i = 0; i < n; ++i) {
-                if(i % 2 == 0 &&  s[i] == '0') {
-                    cnt1++;
-                }
-                if(i % 2 == 0 && s[i] == '1') {
-                    cnt0++;
-                }
-                if (s[i] == '0') {
-                    ++num0;
+        int gap = num1 - num0;
+        if (abs(gap) > 1) return -1;
+        int ans = 0;
+        if (gap == 0) {
+            int error_1first = 0;
+            int error_0first = 0;
+            for (int i = 0; i < len; i += 2) {         //010101
+                if ((i & 1) == 0) {
+                    if (s[i] == '1') ++error_0first;
                 }
             }
-            if (abs((n - num0) - num0) <= 1) {
-                return min(cnt1, cnt0);
-            } else {
-                return -1;
-            }            
-        } else {
-            for (int i = 0; i < n; ++i) {
-                if(i % 2 == 0 &&  s[i] == '0') {
-                    cnt1++;
-                }
-                if(i % 2 == 0 && s[i] == '1') {
-                    cnt0++;
-                }
-                if (s[i] == '0') {
-                    ++num0;
+            for (int i = 0; i < len; i += 2) {         //101010
+                if ((i & 1) == 0) {
+                    if (s[i] == '0') ++error_1first;
                 }
             }
+            ans = min(error_0first, error_1first);
+        } else if (gap == -1) { //0first
+            int error_0 = 0;
+            for (int i = 0; i < len; i += 2) {         //0101010
+                if ((i & 1) == 0) {
+                    if (s[i] == '1') ++error_0;
+                }
+            }
+            ans = error_0;
+        } else {   // 1first
+            int error_1 = 0;
+            for (int i = 0; i < len; i += 2) {         //1010101
+                if ((i & 1) == 0) {
+                    if (s[i] == '0') ++error_1;
+                }
+            }
+            ans = error_1;
         }
+        return ans;
     }
 };
 
-
 int main() {
     Solution test;
-    string s = "100";
+    string s = "0010111";
     cout << test.minSwaps(s);
     return 0;
 }
